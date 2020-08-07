350a0b3f68c69c840c4c674e8a55e1e13ce213e6
Matheus Foresto Moselli <mat.moselli@hotmail.com>
2020-08-07 19:54:23 -0300

    Tipos de Variáveis

---
+++ Course/Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+
+            sbyte n1 = 127;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            char genero = 'F';
+            char letra = '\u0041';
+            string nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f;
+
+            int minInt = int.MinValue;
+            
+
+            Console.WriteLine(completo);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(minInt);
+        }
+    }
+}
