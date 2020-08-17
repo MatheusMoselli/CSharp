391f014166eff5ea831d1e8bc6951032de693c7e
Matheus Foresto Moselli <mat.moselli@hotmail.com>
2020-08-17 09:33:26 -0300

    Exercicio de Métodos Estáticos

---
+++ ExercicioMetodosEstaticos/ExercicioMetodosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioMetodosEstaticos
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double cotacao, double quantia)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF / 100;
+        }
+    }
+}
---
+++ ExercicioMetodosEstaticos/ExercicioMetodosEstaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a coração do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.DolarParaReal(cotacao, dolares).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
